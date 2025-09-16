903fb2a87cfad32f48602ef6e3c67058c4f8efef
Pooja <chothanipooja80@gmail.com>
2025-09-16 21:46:07 +0530

    Revert "updated api model"
    This reverts commit 31f7711e973ee1c8e7e3e35c66d1548c8b2702d4.

--- api/lib/env/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied()
-final class Env{
-  @EnviedField(obfuscate: true)
-  static String GEMINI_API_KEY = _Env.GEMINI_API_KEY;
-}
--- api/routes/_middleware.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:api/src/middleware/chat_reository_provider.dart';
-import 'package:dart_frog/dart_frog.dart';
-
-Handler middleware(Handler handler){
-  return handler.use(requestLogger()).use(chatRepositoryProvider());
-}
